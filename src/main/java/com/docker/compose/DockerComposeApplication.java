bedbdc032a5fc58dc92f672b0a87f1cc780860c7
Elango <paddy.elango@gmail.com>
2024-07-24 09:00:02 -0400

    Docker compose changes

--- src/main/java/com/docker/compose/DockerComposeApplication.java
+++ src/main/java/com/docker/compose/DockerComposeApplication.java
@@ -2,9 +2,17 @@ package com.docker.compose;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DockerComposeApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Welcome to docker compose application";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerComposeApplication.class, args);
